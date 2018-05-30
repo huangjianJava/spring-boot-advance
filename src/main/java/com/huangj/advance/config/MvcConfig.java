60714f8bb73a299ddb0832831b013feee48148f0
huangjian <huangjian@sinter.cn>
2018-05-30 18:17:28 +0800

    code review

---
+++ src/main/java/com/huangj/advance/config/MvcConfig.java
@@ -0,0 +1,20 @@
+package com.huangj.advance.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @author huangj
+ * @Description:  spring boot 扩展 spring mvc 的功能
+ * @date 2018/5/30
+ */
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter{
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("login");
+        registry.addViewController("/login.html").setViewName("login");
+    }
+}
