0f38b96fae436b191b9e65ae41b07dd370c0da30
huangjian <huangjian@sinter.cn>
2018-05-31 18:07:20 +0800

    员工模块

--- src/main/java/com/huangj/advance/dao/EmployeeDao.java
+++ src/main/java/com/huangj/advance/dao/EmployeeDao.java
@@ -7,6 +7,11 @@ import org.springframework.stereotype.Repository;
 import java.util.*;
 import java.util.stream.Collectors;
 
+/**
+ * @author huangj
+ * @Description:  员工 dao
+ * @date 2018/5/31
+ */
 @Repository
 public class EmployeeDao {
 
---
+++ src/test/java/com/huangj/advance/EmployeeTest.java
@@ -0,0 +1,26 @@
+package com.huangj.advance;
+
+import com.huangj.advance.dao.EmployeeDao;
+import com.huangj.advance.model.Employee;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+/**
+ * @author huangj
+ * @Description:
+ * @date 2018/5/31
+ */
+public class EmployeeTest extends BaseTest {
+
+    @Autowired
+    EmployeeDao employeeDao;
+
+    @Test
+    public void testOne(){
+        List<Employee> employees = employeeDao.findAll();
+        System.out.println("size:" + employees.size());
+    }
+
+}
