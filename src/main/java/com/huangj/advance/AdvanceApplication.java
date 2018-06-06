3cfa5ee298b7f3b8ddb71a9e5a6d40796c148d44
huangjian <huangjian@sinter.cn>
2018-06-06 18:25:17 +0800

    code review

--- src/main/java/com/huangj/advance/AdvanceApplication.java
+++ src/main/java/com/huangj/advance/AdvanceApplication.java
@@ -1,11 +1,8 @@
 package com.huangj.advance;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MapperScan(value = "com.huangj.advance.mapper")
 @SpringBootApplication
 public class AdvanceApplication {
 
