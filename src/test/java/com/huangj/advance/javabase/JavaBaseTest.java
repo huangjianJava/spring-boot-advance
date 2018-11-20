8cb63fa2252e208a802848df2e1b7b80cb8eced2
cohuangjian <597112336@qq.com>
2018-11-20 23:34:21 +0800

    code review

--- src/test/java/com/huangj/advance/javabase/JavaBaseTest.java
+++ src/test/java/com/huangj/advance/javabase/JavaBaseTest.java
@@ -1,6 +1,9 @@
 package com.huangj.advance.javabase;
 
+import org.apache.commons.lang3.ArrayUtils;
 import org.junit.Test;
+
+import java.util.Arrays;
 
 /**
  * Author: huangj
@@ -8,6 +11,27 @@ import org.junit.Test;
  * Date: 2018/11/18
  */
 public class JavaBaseTest {
+
+    @Test
+    public void test11() {
+        int[] test = new int[3];
+        Arrays.fill(test,5);
+        for(int t : test){
+            System.out.println(t);
+        }
+
+        int search = Arrays.binarySearch(test,5);
+        System.out.println("search:" + search);
+
+        // ArrayUtils
+        boolean isContain = ArrayUtils.contains(test,5);
+        System.out.println("isContain:" + isContain);
+
+        int[] intArr = ArrayUtils.EMPTY_INT_ARRAY;
+        System.out.println(intArr);
+
+
+    }
 
     @Test
     public void test1() {
