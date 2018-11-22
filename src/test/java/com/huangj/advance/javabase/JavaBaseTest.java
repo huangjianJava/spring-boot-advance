ef0a3206bf1d2e9977b16b3e940a9b8af611c876
cohuangjian <597112336@qq.com>
2018-11-22 22:12:26 +0800

    review

--- src/test/java/com/huangj/advance/javabase/JavaBaseTest.java
+++ src/test/java/com/huangj/advance/javabase/JavaBaseTest.java
@@ -1,10 +1,16 @@
 package com.huangj.advance.javabase;
 
 import org.junit.Test;
+
 import java.util.Arrays;
+
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.util.CollectionUtils;
+
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Author: huangj
@@ -30,8 +36,82 @@ public class JavaBaseTest {
 
         int[] intArr = ArrayUtils.EMPTY_INT_ARRAY;
         System.out.println(intArr);
+
+        System.out.println("测试一下");
+        List<Integer> intList = Arrays.asList(1, 5, 2);
+        //List<Integer> sortedList = intList.stream().sorted().collect(Collectors.toList());
+        Collections.sort(intList);
+        intList.forEach(System.out::println);
     }
 
+    @Test
+    public void test6() {
+        int[][] arr4 = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        System.out.println("原二维数组：");
+        for (int i = 0; i < arr4.length; i++) {
+            for (int j = 0; j < arr4[i].length; j++) { //遍历一维数组
+                System.out.print("\t" + arr4[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println("互调后的二维数组：");
+        for (int i = 0; i < arr4.length; i++) {
+            for (int j = 0; j < arr4[i].length; j++) {
+                System.out.print("\t" + arr4[j][i]);
+            }
+            System.out.println();
+        }
+
+        // java.util 排序
+        int[] arr3 = {6, 3, 8, 2, 9, 1};
+        Arrays.sort(arr3);
+        System.out.println("java.util排序后的数组:");
+        for (int i : arr3) {
+            System.out.println(i);
+        }
+
+        // 直接选择排序
+        System.out.println("=========== 直接选择排序 ============");
+        int[] arr2 = {6, 3, 8, 2, 9, 1};
+        int tempIndex;
+        for (int i = 1; i < arr2.length; i++) {
+            tempIndex = 0;
+            // for 循环找出最大值的下标
+            for (int j = 1; j < arr2.length - i; j++) {
+                if (arr2[j] > arr2[tempIndex]) {
+                    tempIndex = j;
+                }
+            }
+            // 将最大值和第一位交换位置(拿出去)
+            int tempArr = arr2[arr2.length - i];
+            arr2[arr2.length - i] = arr2[tempIndex];
+            arr2[tempIndex] = tempArr;
+        }
+        System.out.println("直接选择排序后的数组:");
+        for (int i : arr2) {
+            System.out.println(i);
+        }
+
+        // 冒泡排序
+        System.out.println("=========== 冒泡排序 ============");
+        int[] arr = {6, 3, 8, 2, 9, 1};
+
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i; j++) {
+                if (arr[j] < arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        boolean sort = ArrayUtils.isSorted(arr);
+        System.out.println("sort:" + sort);
+        System.out.println("冒泡排序后的数组:");
+        for (int i : arr) {
+            System.out.println(i);
+        }
+    }
 
     @Test
     public void test2() {
